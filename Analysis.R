c856841fa4dd2b0a586e5022eb4793283a79152d
ts <ctobiasschnabel@gmail.com>
2022-06-10 21:40:05 +0200

    added paths, packages, dataimport, rough summary

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,25 @@
 #Tobias Schnabel and Obbe Pulles
+#########################HOUSEKEEPING########################
+rm(list = ls(all = TRUE)) ###CLEAR ALL
+# Package names
+packages <- c("data.table", "dplyr", "zoo", "tidyr", "ggplot2", "ggthemes", "scales", "strucchange", "readxl", "summarytools", 
+              "skedastic", "tidyverse", "xtable", "knitr", "stargazer", "patchwork", "remotes", "broom", "purrr")
+# package grateful must be installed by hand# install.packages("remotes")
+# remotes::install_github("Pakillo/grateful")
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+#load packages
+invisible(lapply(packages, library, character.only = TRUE))
+rm(installed_packages)
+
+Paths = c("/Users/ts/Git/Second-Year-Project-II", "#path obbe here")
+names(Paths) = c("ts", "#Sys.info()[7] result for obbe")
+setwd(Paths[Sys.info()[7]])
+
+##import data
+dat = as.data.table(read_excel('Data.xlsx')) 
+stview(dfSummary(dat))
